f23fef051c038d98398928b2a8eb45f4f46b31f0
Ricardo Ferreira <rodrigues.fricardo@gmail.com>
2021-11-02 09:08:18 -0300

    Project created

---
+++ backend/src/test/java/br/com/projecta/devvendas/DevvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projecta.devvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
